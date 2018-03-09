3d9d68a1f4e530fbfc858e8d1860dbf4c2b403fb
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2018-03-09 15:57:18 +0000

    Added in Strings Scene

---
+++ Assets/Scripts/StringHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StringHandler : MonoBehaviour {
+
+    public Text UIText;
+    private StringBuilder sb;
+
+	// Use this for initialization
+	void Start () {
+        //Create a String builder with a current size of
+        //1024 and max capacity of 1024
+        sb = new StringBuilder(1024, 1024);
+	}
+}
